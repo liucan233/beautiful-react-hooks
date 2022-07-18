7dd2dd17be2c67b38db94fcb62047a109394bd5f
313720186@qq.com <313720186@qq.com>
2022-07-19 00:07:54 +0800

    feat: a new useInfiniteScroll

--- src/useInfiniteScroll.ts
+++ src/useInfiniteScroll.ts
@@ -1,35 +1,158 @@
-import { RefObject, useCallback } from 'react'
+import { RefObject, useCallback, useMemo, useRef } from "react";
 
-import useEvent from './useEvent'
-import isFunction from './shared/isFunction'
-import safeHasOwnProperty from './shared/safeHasOwnProperty'
-import createHandlerSetter from './factory/createHandlerSetter'
+import useEvent from "./useEvent";
+import isFunction from "./shared/isFunction";
+import createHandlerSetter from "./factory/createHandlerSetter";
+
+import { CallbackSetter, SomeCallback } from "./shared/types";
+
+/**useInfiniteScroll options */
+interface IOptions {
+  /**monitor which direction of scrolling */
+  direction: "x" | "y" | "both";
+  /**distance from the bottom of the ref element when the callback is called */
+  distance?: number;
+  /**never call preventDefault() */
+  passive?: boolean;
+  /**use capture */
+  capture?: boolean;
+  /**callback function */
+  callback?: SomeCallback<void, any>;
+  /**delay callback */
+  delay?: number;
+}
+
+/**default event options */
+const DEFAULT_EVENT_OPTIONS = { passive: true, capture: false };
+
+/**default element state */
+const DEFAULT_ELEMENT_STATE = {
+  scrollWidth: -1,
+  scrollHeight: -1,
+};
+
+/**reload useInfiniteScroll to add more features */
+interface IUseInfiniteScroll<TElement extends HTMLElement = HTMLElement> {
+  (ref: RefObject<TElement>, delay?: number): CallbackSetter<void>;
+  (ref: RefObject<TElement>, options: IOptions): CallbackSetter<void>;
+}
+
+const useInfiniteScrollByDelay = <TElement extends HTMLElement>(
+  ref: RefObject<TElement>,
+  delay?: number
+): CallbackSetter<void> => {
+  const elementStateRef = useRef(DEFAULT_ELEMENT_STATE);
+
+  const onScroll = useEvent<UIEvent, TElement>(
+    ref,
+    "scroll",
+    DEFAULT_EVENT_OPTIONS
+  );
+
+  const [onScrollEnd, setOnScrollEnd] = createHandlerSetter<void, any>();
+
+  const handleScroll = ({ target, currentTarget }: UIEvent) => {
+    if (
+      target === currentTarget &&
+      target instanceof HTMLElement &&
+      target.scrollHeight !== elementStateRef.current.scrollHeight &&
+      target.scrollHeight < target.clientHeight + target.scrollTop + 10
+    ) {
+      elementStateRef.current = {
+        scrollWidth: -1,
+        scrollHeight: target.scrollHeight,
+      };
+      if (onScrollEnd.current) {
+        delay ? setTimeout(onScrollEnd.current, delay) : onScrollEnd.current();
+      }
+    }
+  };
+
+  onScroll(handleScroll);
+
+  return setOnScrollEnd;
+};
+
+const useInfiniteScrollByOptions = <TElement extends HTMLElement>(
+  ref: RefObject<TElement>,
+  options: IOptions
+): CallbackSetter<void> => {
+  const eventOptionsRef = useRef(DEFAULT_EVENT_OPTIONS);
+  const elementStateRef = useRef(DEFAULT_ELEMENT_STATE);
+
+  if (typeof options === "object") {
+    const { current } = eventOptionsRef;
+    if (
+      options.capture !== current.capture ||
+      options.passive !== current.passive
+    ) {
+      eventOptionsRef.current = { ...DEFAULT_EVENT_OPTIONS };
+      eventOptionsRef.current.capture = options.capture;
+      eventOptionsRef.current.passive = options.passive;
+    }
+  }
+  const onScroll = useEvent<UIEvent, TElement>(
+    ref,
+    "scroll",
+    eventOptionsRef.current
+  );
+  const [onScrollEnd, setOnScrollEnd] = createHandlerSetter<void, any>();
+
+  const handleScroll = ({ target, currentTarget }: UIEvent) => {
+    if (
+      target === currentTarget &&
+      target instanceof HTMLElement &&
+      (target.scrollHeight !== elementStateRef.current.scrollHeight ||
+        target.scrollWidth !== elementStateRef.current.scrollWidth)
+    ) {
+      const delay = options.delay || 250,
+        distance = options.distance || 10,
+        inBottomOfY =
+          target.scrollHeight <
+          target.clientHeight + target.scrollTop + distance,
+        inBottomOfX =
+          target.scrollWidth <
+          target.clientWidth + target.scrollLeft + distance;
+
+      if (
+        (options.direction === "y" && inBottomOfY) ||
+        (options.direction === "x" && inBottomOfX) ||
+        inBottomOfX ||
+        inBottomOfY
+      ) {
+        elementStateRef.current = {
+          scrollWidth: target.scrollWidth,
+          scrollHeight: target.scrollHeight,
+        };
+        if (onScrollEnd.current) {
+          delay
+            ? setTimeout(onScrollEnd.current, delay)
+            : onScrollEnd.current();
+        }
+      }
+    }
+  };
+
+  options.callback && setOnScrollEnd(options.callback);
+  onScroll(handleScroll);
+
+  return setOnScrollEnd;
+};
 
 /**
  * Accepts an HTML Element ref, then returns a function that allows you to handle the infinite
  * scroll for that specific element.
  */
-const useInfiniteScroll = <TElement extends HTMLElement>(ref: RefObject<TElement>, delay = 250) => {
-  const onScroll = useEvent<UIEvent, TElement>(ref, 'scroll', { passive: true })
-  const [onScrollEnd, setOnScrollEnd] = createHandlerSetter<void>()
+const useInfiniteScroll: IUseInfiniteScroll = (ref, options) => {
+  type TElement = typeof ref["current"];
 
-  if (ref && !safeHasOwnProperty(ref, 'current')) {
-    throw new Error('Unable to assign any scroll event to the given ref')
+  if (typeof options === "undefined" || typeof options === "number") {
+    return useInfiniteScrollByDelay<TElement>(ref, options);
+  } else if (options && typeof options === "object") {
+    return useInfiniteScrollByOptions<TElement>(ref, options);
+  } else {
+    throw new TypeError("please make sure to pass in the correct parameters");
   }
+};
 
-  const handleScroll = useCallback(({ target }: UIEvent) => {
-    const el = target as HTMLDivElement
-    if (el) {
-      const isBottom = el.scrollHeight - el.scrollTop === el.clientHeight
-      if (isBottom && isFunction(onScrollEnd?.current)) {
-        setTimeout(onScrollEnd.current, delay)
-      }
-    }
-  }, [])
-
-  onScroll(handleScroll)
-
-  return setOnScrollEnd
-}
-
-export default useInfiniteScroll
+export default useInfiniteScroll;
--- test/useInfiniteScroll.spec.js
+++ test/useInfiniteScroll.spec.js
@@ -1,16 +1,66 @@
-import { cleanup, renderHook } from '@testing-library/react-hooks'
-import useInfiniteScroll from '../dist/useInfiniteScroll'
-import assertHook from './utils/assertHook'
+import { cleanup, renderHook } from "@testing-library/react-hooks";
+import { expect } from "chai";
+import Sinon from "sinon";
+import useInfiniteScroll from "../dist/useInfiniteScroll";
+import assertHook from "./utils/assertHook";
 
-describe('useInfiniteScroll', () => {
-  beforeEach(() => cleanup())
+describe("useInfiniteScroll", () => {
+  beforeEach(() => cleanup());
 
-  assertHook(useInfiniteScroll)
+  assertHook(useInfiniteScroll);
 
-  it('should return an callback setter', () => {
-    const ref = { current: document.createElement('div') }
-    const { result } = renderHook(() => useInfiniteScroll(ref))
+  it("should return an callback setter", () => {
+    const ref = { current: document.createElement("div") };
+    const { result } = renderHook(() => useInfiniteScroll(ref));
 
-    expect(result.current).to.be.a('function')
-  })
-})
+    expect(result.current).to.be.a("function");
+  });
+
+  it("callback when scrolling to the right", () => {
+    const element = document.createElement("div");
+    element.style.height = "100px";
+    element.style.width = "100px";
+    element.style.overflow = "scroll scroll";
+    element.style.display = "block";
+
+    const subElement = document.createElement("div");
+    subElement.style.width = "200px";
+    subElement.style.height = "200px";
+
+    element.appendChild(subElement);
+    document.body.appendChild(element);
+
+    // why result is 0?
+    expect(element.scrollWidth).to.be.equal(200);
+
+    const cb = Sinon.spy();
+
+    const { result } = renderHook(() =>
+      useInfiniteScroll(
+        {
+          current: element,
+        },
+        {
+          passive: true,
+          callback: cb,
+          delay: 200,
+          distance: 100,
+          capture: true,
+          direction: "x",
+        }
+      )
+    );
+
+    element.dispatchEvent(new Event("scroll"));
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (cb.calledOnce) {
+          resolve();
+        } else {
+          reject("called " + cb.callCount + ", expect is 1");
+        }
+      }, 500);
+    });
+  });
+});
